2f826fa6552de37f65816812b18a1160ac8f84c0
Brener Augusto de Oliveira <breneroliveirad@gmail.com>
2021-07-21 22:06:55 -0300

    Algoritmo iniciado

---
+++ Aula 5 - Fila circular (continuação)/L5ex6.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#include "filaCircular6.hpp"
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    Fila f1;
+
+    float numero;
+    int menu;
+    bool controle = false;
+
+    inicializaF(&f1, 8);
+
+    do {
+        cout << "***************** MENU ******************" << endl;
+        cout << "*                                       *" << endl;
+        cout << "* 1. Incluir na fila                    *" << endl;
+        cout << "* 2. Mostrar a fila                     *" << endl;
+        cout << "* 3. Remover da fila                    *" << endl;
+        cout << "* 4. Mostrar dados do arquivo e a soma  *" << endl;
+        cout << "* 5. Sair                               *" << endl;
+        cout << "*                                       *" << endl;
+        cout << "*****************************************" << endl << endl;
+
+        cout << "Escolha uma opcao: ";
+        cin >> menu;
+        fflush(stdin);
+
+        switch(menu) {
+
+            case 1:
+                system("cls");
+
+                cout << "Insira um numero: ";
+                cin >> numero;
+
+                while (numero > 50)
+                {
+                    system("cls");
+                    cout << "Nao sao permitidos maiores que 50." << endl << endl;
+                    system("pause");
+                    system("cls");
+
+                    cout << "Insira um numero menor que 50: ";
+                    cin >> numero;
+                }
+
+                if(enfileiraF(&f1, numero))
+                    cout << "\nNumero inserido." << endl << endl;
+                else
+                    cout << "\nFila cheia." << endl << endl;
+
+                break;
+
+            case 2:
+                system("cls");
+
+                mostraF(&f1);
+                
+                break;
+
+            case 3:
+                system("cls");
+
+                if(desenfileiraF(&f1, &numero)) {
+                    cout << "Numero " << numero << " removido." << endl << endl;
+
+                    ofstream escreve("revisa.txt", ios::app);
+
+                    if(escreve.is_open()) {
+                        escreve << "\n" << numero;
+
+                        escreve.close();
+                    }
+                }
+                else
+                    cout << "Fila vazia." << endl << endl;
+
+                break;
+
+            /*case 4:
+                system("cls");
+
+
+
+                break;*/
+
+            case 5:
+                system("cls");
+
+                /*ofstream escreve("revisa.txt", ios::trunc);
+
+                escreve.close();*/
+
+                cout << "Programa encerrado." << endl << endl;
+
+                break;
+
+            default:
+                system("cls");
+
+                cout << "Opcao invalida." << endl << endl;
+
+                break;
+        }
+
+        system("pause");
+        system("cls");
+
+    } while(menu != 5);
+}
